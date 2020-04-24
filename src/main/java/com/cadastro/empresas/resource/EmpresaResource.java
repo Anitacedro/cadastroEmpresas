27c1e3798e35c24af3ff14e713597363a0ed600a
Ana Clara <anitacedroguima@gmail.com>
2020-04-24 00:58:23 -0300

    Adicionando Swagger a aplicação

--- src/main/java/com/cadastro/empresas/resource/EmpresaResource.java
+++ src/main/java/com/cadastro/empresas/resource/EmpresaResource.java
@@ -2,6 +2,7 @@ package com.cadastro.empresas.resource;
 
 import com.cadastro.empresas.models.Empresa;
 import com.cadastro.empresas.repository.EmpresaRepository;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,31 +16,31 @@ public class EmpresaResource {
     @Autowired
     EmpresaRepository empresaRepository;
 
-    //@ApiOperation(value = "Retorna uma lista de Empresas")
+    @ApiOperation(value = "Retorna uma lista de Empresas")
     @GetMapping("/empresas")
     public List<Empresa> listaEmpresa() {
         return empresaRepository.findAll();
     }
 
-    //@ApiOperation(value = "Retorna uma empresa unica")
+    @ApiOperation(value = "Retorna uma empresa unica")
     @GetMapping("/empresa/{id}")
     public Empresa listaProdutoUnico(@PathVariable(value = "id") long id) {
         return empresaRepository.findById(id);
     }
 
-    //@ApiOperation(value = "Salva uma empresa")
+    @ApiOperation(value = "Salva uma empresa")
     @PostMapping("/empresa")
     public Empresa salvaProduto(@RequestBody @Valid Empresa empresa) {
         return empresaRepository.save(empresa);
     }
 
-    //@ApiOperation(value = "Deleta uma empresa")
+    @ApiOperation(value = "Deleta uma empresa")
     @DeleteMapping("/empresa")
     public void deletaProduto(@RequestBody @Valid Empresa empresa) {
         empresaRepository.delete(empresa);
     }
 
-    //@ApiOperation(value = "Atualiza uma empresa")
+    @ApiOperation(value = "Atualiza uma empresa")
     @PutMapping("/empresa")
     public Empresa atualizaProduto(@RequestBody @Valid Empresa empresa) {
         return empresaRepository.save(empresa);
